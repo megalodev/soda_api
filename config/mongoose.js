6065e9ca89266b0ea3c4bfbe49216482adfe164d
Ahmad Rifa'i <arifai209@gmail.com>
2020-10-28 12:23:06 +0700

    [Add] update account

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,21 +3,20 @@ require('dotenv').config()
 
 const { ENV, DB_URL, DB_URL_TEST } = process.env
 
-mongoose.Promise = Promise
-
-mongoose.connection.on('error', function (err) {
-    console.error(`Database connection error: ${err}`)
-    process.exit(1)
-})
-
-const DB = ENV === 'test' ? DB_URL_TEST : DB_URL
-
 export function dbconn() {
+    const DB = ENV === 'test' ? DB_URL_TEST : DB_URL
+
+    mongoose.Promise = Promise
+
+    mongoose.connection.on('error', function (err) {
+        console.error(`Database connection error: ${err}`)
+        process.exit(1)
+    })
+    
     mongoose.connect(DB, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
-        keepAlive: 1,
         useFindAndModify: false
     }).then(function () { console.log(`Database connected`) })
 
